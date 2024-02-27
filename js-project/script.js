dbcd3550684cdb7ae91eeb6d2a4e971b17154818
miguelfcaiado <caiado.miguelwork@gmail.com>
2024-02-27 15:40:50 +0000

    Language basics in javascript

---
+++ js-project/script.js
@@ -0,0 +1,65 @@
+// console.log('Hello World!');
+
+// var name = 'Alex';
+// console.log(name);
+
+// var schoolTeam = 'basketball';
+// var schoolNumber = 20;
+// console.log(schoolNumber);
+
+
+// var isRegistered = true;
+// console.log(isRegistered);
+
+// var job;
+// console.log(job);
+
+// job = 'student';
+// console.log(job);
+
+// console.log(name + ' ' + schoolNumber);
+
+// var schoolName, hasCourse;
+
+// schoolName = 'OAKAcademy';
+// hasCourse = true;
+
+// console.log(schoolName + ' has 35 courses. Does it have any curse? ' + hasCourse);
+
+// schoolNumber = 'twenty two';
+// console.log(schoolNumber);
+
+// schoolTeam = 'Tennis';
+// console.log(schoolTeam);
+
+// alert(schoolTeam);
+
+// var age = prompt('How old is he?');
+// console.log('Alex is ' + age + ' years old.');
+
+
+////////// BASIC OPERATORS //////////
+// var totalDistance, distanceAlex, distanceSam;
+// totalDistance = 20;
+
+// distanceAlex = totalDistance - 12;
+// console.log(distanceAlex);
+// distanceSam = totalDistance - 7;
+// console.log(distanceSam * 2);
+// console.log(distanceSam / 2);
+
+
+// Logical Operators
+// var distanceToSchool, distanceToHome;
+
+// distanceToHome = 14;
+// distanceToSchool = 6;
+
+// var isCloser = distanceToHome < distanceToSchool;
+// console.log(isCloser);
+
+
+// Boolean Logic
+// var isLoggedIn = true;
+
+
