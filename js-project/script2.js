46569f9bc79d27eacda053ef2c3f5f783c4c4b32
miguelfcaiado <caiado.miguelwork@gmail.com>
2024-03-20 18:09:35 +0000

    DOM

---
+++ js-project/script2.js
@@ -0,0 +1,61 @@
+/*
+var h1Element = document.querySelector('h1');
+
+h1Element.style.color = 'blue';
+
+var isYellow = false;
+var bodyElement = document.querySelector('body');
+setInterval(function() {
+    if(isYellow) {
+        bodyElement.style.background = 'white';
+    }
+    else {
+        bodyElement.style.background = 'yellow';
+    }
+    isYellow = !isYellow;
+}, 1000);
+*/
+
+/*
+var selectWithId = document.getElementById('oakParagraph');
+console.log(selectWithId);
+
+var selectWithClass = document.getElementsByClassName('background');
+console.log(selectWithClass);
+
+var selectWithTag = document.getElementsByTagName('h1');
+console.log(selectWithTag);
+
+var selectWithQueryId = document.querySelector('#oakParagraph');
+console.log(selectWithQueryId);
+
+var selectWithQueryClass = document.querySelector('.background');
+console.log(selectWithQueryClass);
+
+var selectWithQueryAllClass = document.querySelectorAll('.background');
+console.log(selectWithQueryAllClass);
+*/
+
+// var divToManipulate = document.querySelector('.background');
+
+// divToManipulate.style.height = '50px';
+// divToManipulate.style.width = '400px';
+// divToManipulate.style.backgroundColor = 'red';
+// divToManipulate.style.fontSize = '30px';
+// divToManipulate.style.border = '5px solid black';
+
+// divToManipulate.classList.add('addClass');
+// divToManipulate.classList.remove('addClass');
+
+// divToManipulate.classList.toggle('addClass');
+
+// var paragraph = document.querySelector('#oakParagraph');
+
+// paragraph.textContent = 'OAK Academy is the best academy, really';
+
+// paragraph.innerHTML = 'OAK Academy is <strong>great</strong>'
+
+// var myLink = document.querySelector('a');
+
+// myLink.setAttribute('href', 'https://www.facebook.com/');
+// myLink.textContent = 'Go to facebook';
