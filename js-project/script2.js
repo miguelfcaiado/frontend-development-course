9e93e353f44faacba4de4a5baf7a2ef046a63b26
miguelfcaiado <caiado.miguelwork@gmail.com>
2024-03-21 18:12:14 +0000

    Event handlers

--- js-project/script2.js
+++ js-project/script2.js
@@ -59,3 +59,53 @@ console.log(selectWithQueryAllClass);
 
 // myLink.setAttribute('href', 'https://www.facebook.com/');
 // myLink.textContent = 'Go to facebook';
+
+// EVENT HANDLERS
+
+// var button = document.querySelector('#btn1');
+// var title = document.querySelector('#oakTitle');
+
+// button.addEventListener('click', function() {
+//     console.log('You clicked the button');
+//     title.style.backgroundColor = 'red';
+// });
+
+// var listParent = document.querySelector('ul');
+
+// listParent.addEventListener('click', function() {
+//     console.log('You clicked the ul');
+//     listParent.style.backgroundColor = 'blue';
+// });
+
+
+// var listItems = document.querySelectorAll('li');
+
+// console.log('listItems', listItems);
+
+// for(var i = 0; i < listItems.length; i++) {
+//     listItems[i].addEventListener('click', function() {
+//         this.style.color = 'white';
+//     })
+// }
+
+// var oakParagraph = document.querySelector('#oakParagraph');
+
+// function toggleBackground() {
+//     oakParagraph.classList.toggle('toggleBackground');
+// }
+
+// oakParagraph.addEventListener('click', toggleBackground)
+
+var todoListItems = document.querySelectorAll('.todoItem');
+
+for(var i = 0; i < todoListItems.length; i++) {
+    todoListItems[i].addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'blue'
+        this.style.color = 'white'
+    })
+
+    todoListItems[i].addEventListener('mouseout', function() {
+        this.style.backgroundColor = 'white'
+        this.style.color = 'black'
+    })
+}
